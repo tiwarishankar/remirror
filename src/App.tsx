6c058b6d0569e1a9f649de73191c0e105640174f
tiwarishankar <tiwarishankar587@gmail.com>
2024-03-15 22:33:54 +0530

    first rich component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useCallback, useState } from "react";
+import type { RemirrorJSON } from "remirror";
+import { OnChangeJSON } from "@remirror/react";
+import { WysiwygEditor } from "@remirror/react-editors/wysiwyg";
 
-function App() {
-  const [count, setCount] = useState(0)
+const STORAGE_KEY = "remirror-editor-content";
+
+const App: React.FC = () => {
+  const [initialContent] = useState<RemirrorJSON | undefined>(() => {
+    // Retrieve the JSON from localStorage (or undefined if not found)
+    const content = window.localStorage.getItem(STORAGE_KEY);
+    return content ? JSON.parse(content) : undefined;
+  });
+
+  const handleEditorChange = useCallback((json: RemirrorJSON) => {
+    // Store the JSON in localStorage
+    window.localStorage.setItem(STORAGE_KEY, JSON.stringify(json));
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <MyEditor onChange={handleEditorChange} initialContent={initialContent} />
+  );
+};
+
+interface MyEditorProps {
+  onChange: (json: RemirrorJSON) => void;
+  initialContent?: RemirrorJSON;
 }
 
-export default App
+const MyEditor: React.FC<MyEditorProps> = ({ onChange, initialContent }) => {
+  return (
+    <div style={{ padding: 16 }}>
+      <WysiwygEditor
+        placeholder="Enter text..."
+        initialContent={initialContent}
+      >
+        <OnChangeJSON onChange={onChange} />
+      </WysiwygEditor>
+    </div>
+  );
+};
+
+export default App;

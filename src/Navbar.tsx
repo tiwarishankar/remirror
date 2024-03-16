67ecfbde6b4a2b5804b6d394719b9aa15a190c1f
tiwarishankar <tiwarishankar587@gmail.com>
2024-03-16 16:00:40 +0530

    basic editor

---
+++ src/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <nav className="w-[100%] bg-[#000] py-4 px-2 ">
+      <h1 className="font-mono text-m text-[#fff] font-bold">Editor</h1>
+    </nav>
+  );
+};
+
+export default Navbar;
